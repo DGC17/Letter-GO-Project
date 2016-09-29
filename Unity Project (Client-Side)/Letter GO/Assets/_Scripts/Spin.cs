c57827a9d88a18eb2c85862ff4cbbfeca2e56949
David Garia Centelles <David.GarciaC@e-campus.uab.cat>
2016-09-29 18:22:20 +0200

    Unity First Build

---
+++ Unity Project (Client-Side)/Letter GO/Assets/_Scripts/Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// rotate at 90 degrees per second
+		transform.Rotate(Vector3.up * Time.deltaTime*90);
+	}
+}
