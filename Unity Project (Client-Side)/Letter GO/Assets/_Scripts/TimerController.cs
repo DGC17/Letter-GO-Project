0be03945110eb379e1749b9f69b08c373223652c
David Garia Centelles <David.GarciaC@e-campus.uab.cat>
2016-09-30 21:17:12 +0200

    Vuforia Camera + Timer

---
+++ Unity Project (Client-Side)/Letter GO/Assets/_Scripts/TimerController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerController : MonoBehaviour {
+
+	private GameObject timer;
+	private GameObject timer_button;
+	private Image timer_filler;
+	private Text timer_text;
+
+	private bool startTimer;
+	private bool isTimerOn;
+	private bool stopTimer;
+
+	public float timerReduction;
+
+	// Use this for initialization
+	void Start () {
+
+		timer = GameObject.FindGameObjectWithTag ("LetterTimer");
+		timer_button = GameObject.FindGameObjectWithTag ("Timer_Button");
+		timer_filler = GameObject.FindGameObjectWithTag ("Timer_Image").GetComponent<Image>();
+		timer_text = GameObject.FindGameObjectWithTag ("Timer_Text").GetComponent<Text>();
+
+		startTimer = false;
+		isTimerOn = false;
+		stopTimer = false;
+
+		timer.SetActive (false);
+		timer_button.SetActive (true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isTimerOn) {
+
+			float aux = timer_filler.fillAmount;
+			aux -= timerReduction;
+
+			if (aux <= 0) {
+				stopTimer = true;
+			} else {
+				timer_filler.fillAmount = aux;
+			}
+		}
+
+		if (startTimer) {
+			timer.SetActive (true);
+			timer_button.SetActive (false);
+			timer_filler.fillAmount = 1.0f;
+			timer_text.text = generateLetter ();
+			startTimer = false;
+			isTimerOn = true;
+		}
+
+		if (stopTimer) {
+			timer.SetActive (false);
+			timer_button.SetActive (true);
+			timer_filler.fillAmount = 0.0f;
+			timer_text.text = "";
+			stopTimer = false;
+			isTimerOn = false;
+		}
+			
+
+	}
+
+	private string generateLetter () {
+		return "A";
+	}
+
+	public void Play_onClick () {
+		startTimer = true;
+	}
+}
